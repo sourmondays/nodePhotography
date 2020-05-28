20e4440f869159e263f53286df1ee4aa3d134e00
Jasmine Persson <jasmine.persson@hyperisland.se>
2020-05-28 22:19:50 +0200

    Added enpoints for my api:s

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,10 @@ router.get('/', (req, res) => {
   res.send({ status: 'Much welcome to my nodePhotoApp!' });
 });
 
-router.use('/photos', require('./photos'));
-router.use('/albums', require('./albums'));
-router.use('/users', require('./users'));
+router.use('/api/photos', require('./photos'));
+router.use('/api/albums', require('./albums'));
+router.use('/api/users', require('./users'));
+router.use('/register', require('./users'));
 router.use('/profile', [auth.basic], require('./profile'));
 
 
